4f3c4cdc6ce24657b40cff0489188220948f0751
Anji Jonnala <anjir@codeaurora.org>
2012-12-13 14:03:54 +0530

    msm: cpufreq: Implement suspend/resume for cpufreq driver
    Cpufreq suspend variable get's updated in kernel pm notifier
    callback which gets called in process context. This will create
    window where cpufreq governor may try to change the frequency
    before suspend variable is updated while resume.
    
    Define suspend/resume for cpufreq driver to update suspend variable
    in atomic context.
    
    Don't update the policy->cur upon cpufreq driver failure.
    
    CRs-fixed: 423791
    Change-Id: Id78705f5c3b8bf801c6253e9362299280903769f
    Signed-off-by: Anji Jonnala <anjir@codeaurora.org>
    
    Conflicts:
    
    	drivers/cpufreq/cpufreq_ondemand.c

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -1008,10 +1008,20 @@ static void dbs_refresh_callback(struct work_struct *unused)
 	}
 
 	if (policy->cur < policy->max) {
+<<<<<<< HEAD
 		if (__cpufreq_driver_target(policy, policy->max,
 					CPUFREQ_RELATION_L) >= 0) {
 			policy->cur = policy->max;
 		}
+=======
+		/*
+		 * Arch specific cpufreq driver may fail.
+		 * Don't update governor frequency upon failure.
+		 */
+		if (__cpufreq_driver_target(policy, policy->max,
+					CPUFREQ_RELATION_L) >= 0)
+			policy->cur = policy->max;
+>>>>>>> ebc453c... msm: cpufreq: Implement suspend/resume for cpufreq driver
 
 		this_dbs_info->prev_cpu_idle = get_cpu_idle_time(cpu,
 				&this_dbs_info->prev_cpu_wall);
